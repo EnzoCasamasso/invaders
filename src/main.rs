632394b1b16406828052c648140b2b6f8ed76b05
Enzo <enzocasamasso@gmail.com>
2024-02-29 21:09:02 -0300

    add main configurations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    event::{self, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType, EnterAlternateScreen},
+};
+use ratatui::prelude::*;
+use std::io;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, Clear(ClearType::All), EnterAlternateScreen)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    //mainloop
+
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        crossterm::terminal::LeaveAlternateScreen,
+        Clear(ClearType::All)
+    )?;
+    Ok(())
 }
